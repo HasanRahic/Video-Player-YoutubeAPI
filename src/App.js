721605230706509cf323baf8f28472f20368068f
Hasan Rahic <haso.raha10@gmail.com>
2024-03-31 03:59:28 +0200

    starting new project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    <p>hello world</p>
-    </>
+    <form>
+      <div className="main">
+        <div className="navbar">
+          <div className="navbar home-navbar">Home</div>
+          <div className="navbar upload">Upload</div>
+        </div>
+        <div className="home">
+          <p>this that this that</p>
+        </div>
+      </div>
+    </form>
   );
 }
 
