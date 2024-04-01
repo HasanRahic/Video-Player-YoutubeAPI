721d1690cd0437b301245f23519109d38da351d0
Hasan Rahic <haso.raha10@gmail.com>
2024-04-01 20:52:55 +0200

    adding new files

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <form>
       <div className="main">
         <div className="navbar">
-          <div className="navbar home-navbar">Home</div>
-          <div className="navbar upload">Upload</div>
+          <div className="home-navbar">Home</div>
+          <div className="upload">Upload</div>
         </div>
         <div className="home">
           <p>this that this that</p>
