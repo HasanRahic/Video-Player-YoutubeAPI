3d4edf4aa0d0caf6b641486f0d13e663e06d67ae
Hasan Rahic <haso.raha10@gmail.com>
2024-04-04 03:08:46 +0200

    made navbar for webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
+import React from 'react';
 import './App.css';
+import './Components/Navbar/Navbar'
+import Navbar from './Components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
 
 function App() {
   return (
-    <form>
-      <div className="main">
-        <div className="navbar">
-          <div className="home-navbar">Home</div>
-          <div className="upload">Upload</div>
-        </div>
-        <div className="home">
-          <div className="video">this video</div>
-          <div className="video">this video</div>
-          <div className="video">this video</div>
-          <div className="video">this video</div>
-          <div className="video">this video</div>
-          <div className="video">this video</div>
-          <div className="video">this video</div>
-          <div className="video">this video</div>
-        </div>
-      </div>
-    </form>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Video/:categoryId/:videoId' element={<Video/>} />
+      </Routes>
+    </div>
   );
 }
 
