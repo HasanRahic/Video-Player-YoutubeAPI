b2d0606ecdd2cfb2ba8537822448a70a9e456bb3
Hasan Rahic <haso.raha10@gmail.com>
2024-04-09 03:58:15 +0200

    adding API to the code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Feed from '../../Components/Feed/Feed'
 
 const Home  = ({sideBar}) => {
+
+  const [category, setCategory] = useState(0);
+
+
   return (
     <>
-      <Sidebar sideBar={sideBar} />
+      <Sidebar sideBar={sideBar} category={category} setCategory={setCategory}/>
       <div className={`container ${sideBar ? "" : 'large-container'}`}>
-        <Feed />  
+        <Feed category={category}/>  
       </div> 
     </>
   )
--- src/data.js
+++ src/data.js
@@ -1,1 +1,11 @@
 export const API_KEY = 'AIzaSyCcIY8NoU5pTLvYU-x6omUVdXywtQ1q2pQ';
+
+export const value_converter = (value) =>{
+    if(value >= 1000000){
+        return Math.floor(value/1000000)+"M";
+    } else if(value >= 1000){
+        return Math.floor(value/1000)+"K";
+    } else{
+        return value;
+    }
+}
