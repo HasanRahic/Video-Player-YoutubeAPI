7bc7976713c0adaac3976a22cdb5b099fd2f0297
Hasan Rahic <haso.raha10@gmail.com>
2024-04-12 15:37:36 +0200

    adding titles views and post date to played video

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
 import video1 from '../../Assets/video.mp4'
 import like from '../../Assets/like.png'
@@ -7,15 +7,29 @@ import share from '../../Assets/share.png'
 import save from '../../Assets/save.png'
 import jack from '../../Assets/jack.png'
 import user_profile from '../../Assets/user_profile.jpg'
+import { API_KEY, value_converter } from '../../data'
+import moment from 'moment'
 
 const PlayVideo = ({videoId}) => {
+
+  const [apiData, setApiData] = useState(null);
+
+  const fetchData = async () => {
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+    await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]))
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
   return (
     <div className='play-video'>
         {/* <video src={video1} controls autoPlay muted></video> */}
         <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <h3>Best video to learn coding that help you to be a web developer</h3>
+        <h3>{apiData?apiData.snippet.title:"Title here"}</h3>
         <div className="play-video-info">
-            <p>87211 views &bull; 2 days ago</p>
+            <p>{apiData?value_converter(apiData.statistics.viewCount):"16K"} views &bull; {moment(apiData.snippet.publishedAt).fromNow()}</p>
             <div>
                 <span><img src={like} alt="" /> 542</span>
                 <span><img src={dislike} alt="" /> 22</span>
