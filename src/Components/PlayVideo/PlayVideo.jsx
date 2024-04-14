403a43458812b683497224abc570cd0cade7d6f2
Hasan Rahic <haso.raha10@gmail.com>
2024-04-14 14:09:54 +0200

    connecting api to comment section

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -10,37 +10,56 @@ import moment from 'moment'
 
 const PlayVideo = ({videoId}) => {
 
-  const [apiData, setApiData] = useState(false);
-  const [channelData, setChannelData] = useState(false);
-
+  const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentData, setCommentData] = useState([]);
+  
   const fetchDataVideo = async () => {
-    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
-    await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]))
-  }
-
+    try {
+      const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+      const response = await fetch(videoDetails_url);
+      const data = await response.json();
+      if (data.items && data.items.length > 0) {
+        setApiData(data.items[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching video data:", error);
+    }
+  };
+  
   const fetchOtherData = async () => {
-    const channelDetails_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
-    await fetch(channelDetails_url).then(res=>res.json()).then(data => setChannelData(data.items[0]))
-  }
-
-
-  useEffect(()=>{
+    if (apiData && apiData.snippet && apiData.snippet.channelId) {
+      try {
+        const channelDetails_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
+        const response = await fetch(channelDetails_url);
+        const data = await response.json();
+        if (data.items && data.items.length > 0) {
+          setChannelData(data.items[0]);
+        }
+        const comment_url = ` https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`
+        await fetch(comment_url).then(res=>res.json()).then(data=>setCommentData(data.items))
+      } catch (error) {
+        console.error("Error fetching channel data:", error);
+      }
+    }
+  };
+  
+  useEffect(() => {
     fetchDataVideo();
-  },[])
-
-  useEffect(()=>{
+  }, []);
+  
+  useEffect(() => {
     fetchOtherData();
-  },[apiData])
-
+  }, [apiData]);
   return (
     <div className='play-video'>
         {/* <video src={video1} controls autoPlay muted></video> */}
         <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <h3>{apiData?apiData.snippet.title:"Title here"}</h3>
         <div className="play-video-info">
-            <p>{apiData?value_converter(apiData.statistics.viewCount):"16K"} views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
+            <p>{apiData ? value_converter(apiData.statistics.viewCount) : "16K"} views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}</p>
             <div>
-                <span><img src={like} alt="" /> {apiData?value_converter(apiData.statistics.likeCount) : 1}</span>
+                <span><img src={like} alt="" /> {apiData ? value_converter(apiData.statistics.likeCount) : 1}</span>
                 <span><img src={dislike} alt="" /> 22</span>
                 <span><img src={share} alt="" /> Share</span>
                 <span><img src={save} alt="" /> Save</span>
@@ -48,95 +67,35 @@ const PlayVideo = ({videoId}) => {
         </div>
       <hr />
       <div className="publisher">
-        <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
+        <img src={channelData ? channelData.snippet.thumbnails.default.url : user_profile} alt="" />
         <div>
           <p>{apiData?apiData.snippet.channelTitle:"Chennel title"}</p>
-          <span>{channelData?value_converter(channelData.statistics.subscriberCount):"1M"} Subscribers</span>
+          <span>{channelData ? value_converter(channelData.statistics.subscriberCount) : "1M"} Subscribers</span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-description">
-        <p>{apiData?apiData.snippet.description.slice(0,250):"Description here"}</p>
+        <p>{apiData ? apiData.snippet.description.slice(0,250) : "Description here"}</p>
         <hr />
-        <h4>{apiData?value_converter(apiData.statistics.commentCount) : 0}</h4>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>Nikola Nikolshon <span>1 day ago</span></h3>
-            <p>awesome video again thx for this i have used Tailwind on a project before but yeah it felt overdoing it for what was required at the time, i still might try it again</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>13</span>
-              <img src={dislike} alt="" />
-              <span>1</span>
+        <h4>{apiData ? value_converter(apiData.statistics.commentCount) : 0}</h4>
+        {commentData.map((item, index)=>{
+
+          return(
+            <div key={index} className="comment">
+              <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+              <div>
+                <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>1 day ago</span></h3>
+                <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
+                <div className="comment-action">
+                  <img src={like} alt="" />
+                  <span>{value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                  <img src={dislike} alt="" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>Nikola Nikolshon <span>1 day ago</span></h3>
-            <p>awesome video again thx for this i have used Tailwind on a project before but yeah it felt overdoing it for what was required at the time, i still might try it again</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>13</span>
-              <img src={dislike} alt="" />
-              <span>1</span>
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>Nikola Nikolshon <span>1 day ago</span></h3>
-            <p>awesome video again thx for this i have used Tailwind on a project before but yeah it felt overdoing it for what was required at the time, i still might try it again</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>13</span>
-              <img src={dislike} alt="" />
-              <span>1</span>
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>Nikola Nikolshon <span>1 day ago</span></h3>
-            <p>awesome video again thx for this i have used Tailwind on a project before but yeah it felt overdoing it for what was required at the time, i still might try it again</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>13</span>
-              <img src={dislike} alt="" />
-              <span>1</span>
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>Nikola Nikolshon <span>1 day ago</span></h3>
-            <p>awesome video again thx for this i have used Tailwind on a project before but yeah it felt overdoing it for what was required at the time, i still might try it again</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>13</span>
-              <img src={dislike} alt="" />
-              <span>1</span>
-            </div>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>Nikola Nikolshon <span>1 day ago</span></h3>
-            <p>awesome video again thx for this i have used Tailwind on a project before but yeah it felt overdoing it for what was required at the time, i still might try it again</p>
-            <div className="comment-action">
-              <img src={like} alt="" />
-              <span>13</span>
-              <img src={dislike} alt="" />
-              <span>1</span>
-            </div>
-          </div>
-        </div>
+          )
+        })}
+        
       </div>
     </div>
   )
