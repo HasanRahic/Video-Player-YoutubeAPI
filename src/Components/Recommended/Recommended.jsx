65938762109ffbb30d9b35635749c678efefe488
Hasan Rahic <haso.raha10@gmail.com>
2024-04-16 16:15:05 +0200

    getting recommended title thumbnails and views through api

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,21 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './Recommended.css'
-import thumbnail1 from '../../Assets/thumbnail1.png'
-import thumbnail2 from '../../Assets/thumbnail2.png'
-import thumbnail3 from '../../Assets/thumbnail3.png'
-import thumbnail4 from '../../Assets/thumbnail4.png'
-import thumbnail5 from '../../Assets/thumbnail5.png'
-import thumbnail6 from '../../Assets/thumbnail6.png'
-import thumbnail7 from '../../Assets/thumbnail7.png'
-import thumbnail8 from '../../Assets/thumbnail8.png'
-import { API_KEY } from '../../data'
+import { API_KEY, value_converter } from '../../data'
 
 const Recommended = ({categoryId}) => {
 
     const [apiData, setApiData] = useState([]);
 
     const fetchData = async() =>{
-        const relatedVideo_url = ` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+        const relatedVideo_url = ` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
         await fetch(relatedVideo_url).then(res=>res.json()).then(data=>setApiData(data.items));
     }
 
@@ -25,70 +17,19 @@ const Recommended = ({categoryId}) => {
 
   return (
     <div className='recommended'>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail2} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail3} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail4} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail5} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail6} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail7} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail8} alt="" />
-            <div className="vid-info">
-                <h4>Best video to learn coding that help you to be a web developer</h4>
-                <p>Mr Beast</p>
-                <p>84k views</p>
-            </div>
-        </div>
+        {apiData.map((item, index)=>{
+            return (
+                <div key={index} className="side-video-list">
+                    <img src={item.snippet.thumbnails.medium.url} alt="" />
+                    <div className="vid-info">
+                        <h4>{item.snippet.title}</h4>
+                        <p>{item.snippet.channelTitle}</p>
+                        <p>{value_converter(item.statistics.viewCount)}</p>
+                    </div>
+                </div>
+            )
+        })}
+        
     </div>
   )
 }
